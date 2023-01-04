79330ea00273fba8dd482defeacff24c26fde696
HanRachid <handaouirachid@gmail.com>
2023-01-04 15:04:26 +0100

    add linkledlist class with its node

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,23 @@ import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import { setupCounter } from "./counter";
 import { BalancedBST } from "./Tree/BalancedBST/BalancedBST";
+import { LinkedList } from "./Lists/LinkedList";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+<div>
+<a href="https://vitejs.dev" target="_blank">
+  <img src="/vite.svg" class="logo" alt="Vite logo" />
+</a>
+<a href="https://www.typescriptlang.org/" target="_blank">
+  <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+</a>
+<h1>Vite + TypeScript</h1>
+<div class="card">
+  <button id="counter" type="button"></button>
+</div>
+<p class="read-the-docs">
+  Click on the Vite and TypeScript logos to learn more
+</p>
+</div>
 `;
 
 setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
@@ -39,3 +40,9 @@ testtree.reBalance();
 
 testtree.prettyPrint();
 console.log(testtree.isBalanced());
+
+let testlist = new LinkedList();
+testlist.append(2);
+testlist.append(3);
+testlist.prepend(3);
+console.log(testlist.toString());
